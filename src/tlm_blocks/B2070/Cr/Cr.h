d6599c42468782c81747b518f10cb3fdf2033d47
caron.louis@gmail.com <caron.louis@gmail.com@9d2795e5-a26c-bf4f-ff81-838b8f8d5f12>
2010-08-23 09:53:27 +0000

    Start the development of new HW block

--- src/tlm_blocks/B2070/Cr/Cr.h
+++ src/tlm_blocks/B2070/Cr/Cr.h
@@ -3,7 +3,7 @@
 
 #include "SimpleSlave.h"
 
-struct Cr : sc_core::sc_module
+struct Cr : SimpleSlave
 {
     /// Constructor
     Cr(sc_core::sc_module_name name);
@@ -11,17 +11,32 @@ struct Cr : sc_core::sc_module
     /// Registers definition
     enum
     {
-        REG_INTCTRL_IRQEN_SET  = 0,
-        REG_INTCTRL_IRQEN_CLR  = 1,
-        REG_INTCTRL_IRQST_RAW  = 2,
-        REG_INTCTRL_FIQST_RAW  = 3,
-        REG_INTCTRL_WFI        = 4,
+        REG_CR_CLK_DIV_SEL  = 0,
 
-        REG_SIZE               = 256
+        REG_CR_COUNT        = 61
     };
 
     /// Registers content
-    uint32_t m_registers[REG_SIZE];
+    uint32_t m_reg[REG_CR_COUNT];
+
+    /// Override the virtual function
+    void slave_b_transport( tlm::tlm_generic_payload& trans, sc_core::sc_time& delay );
+
+protected:
+    /**
+     * Register read function
+     * @param[in] offset Offset of the register to read
+     * @return The value read
+     */
+    uint32_t
+    reg_rd(uint32_t offset);
+
+    /**
+     * Register write function
+     * @param[in] offset Offset of the register to read
+     */
+    void
+    reg_wr(uint32_t offset, uint32_t value);
 
 };
 
