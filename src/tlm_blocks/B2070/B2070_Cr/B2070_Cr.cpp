61fd241fcd8ffac6f57f23960e44e89557bd8666
caron.louis@gmail.com <caron.louis@gmail.com@9d2795e5-a26c-bf4f-ff81-838b8f8d5f12>
2010-08-23 12:03:35 +0000

    Move constructor to header file

--- src/tlm_blocks/B2070/B2070_Cr/B2070_Cr.cpp
+++ src/tlm_blocks/B2070/B2070_Cr/B2070_Cr.cpp
@@ -7,33 +7,6 @@
 // using this namespace to simplify streaming
 using namespace std;
 
-B2070_Cr::B2070_Cr(sc_core::sc_module_name name) : SimpleSlave(name, m_reg, REG_CR_COUNT*4)
-{
-}
-
-void B2070_Cr::slave_b_transport( tlm::tlm_generic_payload& trans, sc_core::sc_time& delay )
-{
-    TLM_WORD_SANITY(trans);
-
-    // retrieve the required parameters
-    uint32_t* ptr = reinterpret_cast<uint32_t*>(trans.get_data_ptr());
-
-    if (trans.get_command() == tlm::TLM_READ_COMMAND)
-    {
-        *ptr = reg_rd(trans.get_address());
-    }
-    else
-    {
-        reg_wr(trans.get_address(), *ptr);
-    }
-
-    // there was no error in the processing
-    trans.set_response_status( tlm::TLM_OK_RESPONSE );
-
-    return;
-}
-
-
 uint32_t B2070_Cr::reg_rd(uint32_t offset)
 {
     uint32_t result;
