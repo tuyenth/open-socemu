82f18eac43e42e222fad816dab61187d2826984d
caron.louis@gmail.com <caron.louis@gmail.com@9d2795e5-a26c-bf4f-ff81-838b8f8d5f12>
2010-08-27 14:35:01 +0000

    Use Peripheral and Interrupt classes

--- src/tlm_blocks/Mc13224v/Gpio/Gpio.h
+++ src/tlm_blocks/Mc13224v/Gpio/Gpio.h
@@ -1,52 +1,36 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
-// necessary define for processes in simple_target_socket
-#define SC_INCLUDE_DYNAMIC_PROCESSES
-
-// obvious inclusion
-#include "systemc"
-
-// not so obvious inclusions
-#include "tlm.h"
-#include "tlm_utils/simple_target_socket.h"
+// this is a peripheral
+#include "Peripheral.h"
 
 // include the registers definition
 #include "reg_gpio.h"
 
 /// Interrupt Controller block model
-struct Gpio : sc_core::sc_module
+struct Gpio : Peripheral<REG_GPIO_COUNT>
 {
-    /// TLM-2 slave socket to handle bus accesses
-    tlm_utils::simple_target_socket<Gpio> reg_s_socket;
-
-    // Not necessary if this module does not have a thread
-//    SC_HAS_PROCESS(Gpio);
-
     /// Constructor
-    Gpio(sc_core::sc_module_name name);
+    Gpio(sc_core::sc_module_name name)
+    : Peripheral<REG_GPIO_COUNT>(name)
+    {
+        // initialized the register access
+        reg_gpio = &(m_reg[0]);
+    }
 
-    /// TLM-2 socket blocking method
-    virtual void reg_s_b_transport( tlm::tlm_generic_payload& trans, sc_core::sc_time& delay );
+private:
+    /** Register read function
+     * @param[in] offset Offset of the register to read
+     * @return The value read
+     */
+    uint32_t
+    reg_rd(uint32_t offset);
 
-    /// TLM-2 socket non blocking path
-    virtual tlm::tlm_sync_enum reg_s_nb_transport_fw( tlm::tlm_generic_payload& trans,
-            tlm::tlm_phase& phase, sc_core::sc_time& delay );
-
-    /// TLM-2 socket debug path
-    virtual unsigned int reg_s_transport_dbg(tlm::tlm_generic_payload& trans);
-
-    /// Read access to the registers
-    uint32_t reg_rd(uint32_t offset);
-
-    /// Write access to the registers
-    void reg_wr(uint32_t offset, uint32_t value);
-
-    /// Registers content
-    uint32_t m_reg[REG_GPIO_COUNT];
-
-    /// Indicate if busy for sanity check
-    bool m_free;
+    /** Register write function
+     * @param[in] offset Offset of the register to read
+     */
+    void
+    reg_wr(uint32_t offset, uint32_t value);
 };
 
 #endif /*GPIO_H_*/
--- src/tlm_blocks/Mc13224v/Itc/Itc.cpp
+++ src/tlm_blocks/Mc13224v/Itc/Itc.cpp
@@ -10,82 +10,8 @@ uint32_t* reg_itc;
 // using this namespace to simplify streaming
 using namespace std;
 
-Itc::Itc(sc_core::sc_module_name name)
-: reg_s_socket("reg_s_socket")
-, irq_m_socket("irq_m_socket")
-, fiq_m_socket("fiq_m_socket")
-, m_free(true)
-{
-    // force the default values of the FIQ transaction
-    fiq_pl.set_streaming_width(4);
-    fiq_pl.set_byte_enable_ptr(0);
-    fiq_pl.set_dmi_allowed(false);
-    // force the default values of the IRQ transaction
-    irq_pl.set_streaming_width(4);
-    irq_pl.set_byte_enable_ptr(0);
-    irq_pl.set_dmi_allowed(false);
-
-    // initialized the register access
-    reg_itc = &(m_reg[0]);
-
-    // clear all the registers
-    memset(m_reg, 0, sizeof(m_reg));
-
-    // Register callbacks for incoming interface method calls
-    reg_s_socket.register_b_transport(this, &Itc::reg_s_b_transport);
-    reg_s_socket.register_nb_transport_fw(this, &Itc::reg_s_nb_transport_fw);
-    reg_s_socket.register_transport_dbg(this, &Itc::reg_s_transport_dbg);
-
-    for (uint8_t i = 0; i < NUM_INT; i++)
-    {
-        char txt[20];
-        sprintf(txt, "int_s_socket[%d]", i);
-        int_s_socket[i] = new tlm_utils::simple_target_socket_tagged<Itc>(txt);
-        // only blocking method is supported
-        int_s_socket[i]->register_b_transport(this, &Itc::int_s_b_transport, i);
-    }
-}
-
-void Itc::reg_s_b_transport( tlm::tlm_generic_payload& trans, sc_core::sc_time& delay )
-{
-    TLM_WORD_SANITY(trans);
-
-    // retrieve the required parameters
-    uint32_t* ptr = reinterpret_cast<uint32_t*>(trans.get_data_ptr());
-
-    if (trans.get_command() == tlm::TLM_READ_COMMAND)
-    {
-        *ptr = reg_rd(trans.get_address());
-    }
-    else
-    {
-        reg_wr(trans.get_address(), *ptr);
-    }
-
-    // there was no error in the processing
-    trans.set_response_status( tlm::TLM_OK_RESPONSE );
-
-    return;
-}
-
-tlm::tlm_sync_enum Itc::reg_s_nb_transport_fw( tlm::tlm_generic_payload& trans,
-        tlm::tlm_phase& phase, sc_core::sc_time& delay )
-{
-    SC_REPORT_FATAL("TLM-2", "Non blocking not yet implemented");
-    return tlm::TLM_COMPLETED;
-}
-
-
-unsigned int Itc::reg_s_transport_dbg(tlm::tlm_generic_payload& trans)
-{
-    // sanity check
-    TLM_TRANS_SANITY(trans);
-
-    // execute the debug command
-    TLM_DBG_EXEC(trans, m_reg, sizeof(m_reg));
-}
-
-uint32_t Itc::reg_rd(uint32_t offset)
+uint32_t
+Itc::reg_rd(uint32_t offset)
 {
     uint32_t result;
     // retrieve the required parameters
@@ -93,13 +19,9 @@ uint32_t Itc::reg_rd(uint32_t offset)
 
     // sanity check
     assert(index < REG_ITC_COUNT);
-    assert(m_free);
-
-    // mark as busy
-    m_free = false;
 
     // internal delay
-    wait(100, sc_core::SC_NS);
+    this->delay();
 
     switch (index)
     {
@@ -111,30 +33,24 @@ uint32_t Itc::reg_rd(uint32_t offset)
 
     default:
         // read the register value
-        result = this->m_reg[index];
+        result = m_reg[index];
         break;
     }
-
-    // mark as free
-    m_free = true;
 
     return result;
 }
 
-void Itc::reg_wr(uint32_t offset, uint32_t value)
+void
+Itc::reg_wr(uint32_t offset, uint32_t value)
 {
     // retrieve the required parameters
     uint32_t index = offset/4;
 
     // sanity check
     assert(index < REG_ITC_COUNT);
-    assert(m_free);
-
-    // mark as busy
-    m_free = false;
 
     // internal delay
-    wait(100, sc_core::SC_NS);
+    this->delay();
 
     switch (index)
     {
@@ -162,41 +78,10 @@ void Itc::reg_wr(uint32_t offset, uint32_t value)
 
     // after every write, check if there is an interrupt to generate or clear
     this->check_int();
-
-    // mark as free
-    m_free = true;
 }
 
-void Itc::int_s_b_transport( int id, tlm::tlm_generic_payload& trans, sc_core::sc_time& delay )
-{
-    TLM_INT_SANITY(trans);
-
-    // retrieve the required parameters
-    uint32_t* ptr = reinterpret_cast<uint32_t*>(trans.get_data_ptr());
-
-    // sanity check
-    assert(id < NUM_INT);
-
-    if (*ptr == 1)
-    {
-        // set the register value
-        intsrc_set(intsrc_get() | (1 << id));
-    }
-    else
-    {
-        // clear the register value
-        intsrc_set(intsrc_get() & (~(1 << id)));
-    }
-
-    // check interrupts
-    check_int();
-
-    trans.set_response_status( tlm::TLM_OK_RESPONSE );
-
-    return;
-}
-
-void Itc::check_int()
+void
+Itc::check_int()
 {
     uint32_t pending_fiq;
     uint32_t pending_irq;
@@ -253,21 +138,50 @@ void Itc::check_int()
     // check if there are pending interrupts
     if (fipend_get() != 0)
     {
-        TLM_INT_SET(fiq_m_socket, fiq_pl, fiq_delay);
-        TLM_INT_CLR(irq_m_socket, irq_pl, irq_delay);
+        this->fiq.set();
+        this->irq.clear();
     }
     else
     {
-        TLM_INT_CLR(fiq_m_socket, fiq_pl, fiq_delay);
+        this->fiq.clear();
 
         if (nipend_get() != 0)
         {
-            TLM_INT_SET(irq_m_socket, irq_pl, irq_delay);
+            this->irq.set();
         }
         else
         {
-            TLM_INT_CLR(irq_m_socket, irq_pl, irq_delay);
+            this->irq.clear();
         }
     }
 }
 
+void
+Itc::interrupt_set(void* opaque)
+{
+    int id = (int)opaque;
+
+    // sanity check
+    assert(id < NUM_INT);
+
+    // set the register value
+    intsrc_set(intsrc_get() | (1 << id));
+
+    // check if interrupt configuration has changed
+    check_int();
+}
+
+void
+Itc::interrupt_clr(void* opaque)
+{
+    int id = (int)opaque;
+
+    // sanity check
+    assert(id < NUM_INT);
+
+    // clear the register value
+    intsrc_set(intsrc_get() & (~(1 << id)));
+
+    // check if interrupt configuration has changed
+    check_int();
+}
