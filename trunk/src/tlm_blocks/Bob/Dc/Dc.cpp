75d43ebfda0042eaee50ae68a7185b59e37dc5d0
caron.louis@gmail.com <caron.louis@gmail.com@9d2795e5-a26c-bf4f-ff81-838b8f8d5f12>
2010-11-15 05:14:17 +0000

    Add timer functionality

--- trunk/src/tlm_blocks/Bob/Dc/Dc.cpp
+++ trunk/src/tlm_blocks/Bob/Dc/Dc.cpp
@@ -10,6 +10,7 @@ using namespace std;
 uint32_t
 Dc::reg_rd(uint32_t offset)
 {
+    sc_core::sc_time sctime;
     uint32_t result;
     // retrieve the required parameters
     uint32_t index = offset/4;
@@ -22,9 +23,20 @@ Dc::reg_rd(uint32_t offset)
 
     switch (index)
     {
+    case REG_DC_NBTC_CLK:
+        sctime = sc_core::sc_time_stamp();
+        result = (sctime.value() / 312500) & 0x0FFFFFFF;
+        TLM_DBG("nbtc_clk = 0x%08X", result);
+        break;
+    case REG_DC_NBTC_PCLK:
+        sctime = sc_core::sc_time_stamp();
+        result = ((sctime.value() % 312500) / 1000 ) & 0x1FF;
+        TLM_DBG("nbtc_pclk = 0x%08X", result);
+        break;
     default:
         // read the register value
         result = m_reg[index];
+        TLM_DBG("m_reg[%d] = 0x%08X", index, result);
         break;
     }
 
