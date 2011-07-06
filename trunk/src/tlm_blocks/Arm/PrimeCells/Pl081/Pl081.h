6aef80de22a06f7d04e9129d227301e7a9f85a72
caron.louis@gmail.com <caron.louis@gmail.com@9d2795e5-a26c-bf4f-ff81-838b8f8d5f12>
2011-07-06 07:47:20 +0000

    Start SMDMAC implementation

--- trunk/src/tlm_blocks/Arm/PrimeCells/Pl081/Pl081.h
+++ trunk/src/tlm_blocks/Arm/PrimeCells/Pl081/Pl081.h
@@ -5,6 +5,7 @@
 
 #include "utils.h"
 #include "Generic/Peripheral/Peripheral.h"
+#include "Generic/IntMaster/IntMaster.h"
 
 /// Registers definition
 enum
@@ -83,11 +84,59 @@ struct Pl081 : Peripheral<REG_PL081_COUNT>
     /// Constructor
     Pl081(sc_core::sc_module_name name)
     : Peripheral<REG_PL081_COUNT>(name)
+    , intsource("intsource")
+    , master_socket("master_socket")
     {
+        // force the default values of the BUS transaction
+        master_b_pl.set_streaming_width(4);
+        master_b_pl.set_byte_enable_ptr(0);
+        master_b_pl.set_dmi_allowed(false);
+        // register callbacks for incoming interface method calls
+        master_socket.register_nb_transport_bw(this, &Pl081::master_nb_transport_bw);
+        master_socket.register_invalidate_direct_mem_ptr(this, &Pl081::master_invalidate_direct_mem_ptr);
+
         // initialize the registers content
+        m_reg[REG_PL081_DMACPERIPHID0] = 0x81;
+        m_reg[REG_PL081_DMACPERIPHID1] = 0x10;
+        m_reg[REG_PL081_DMACPERIPHID2] = 0x05;
+        m_reg[REG_PL081_DMACPERIPHID3] = 0x00;
+        m_reg[REG_PL081_DMACPCELLID0] = 0x0D;
+        m_reg[REG_PL081_DMACPCELLID1] = 0xF0;
+        m_reg[REG_PL081_DMACPCELLID2] = 0x05;
+        m_reg[REG_PL081_DMACPCELLID3] = 0xB1;
     }
 
+    /** Bind a slave socket to the local master socket
+     * @param[in, out] slave_socket TLM-2 slave socket to bind to the master socket of the peripheral
+     */
+    void
+    bind(tlm::tlm_target_socket<>& slave_socket)
+    {
+        // hook the slave socket
+        this->master_socket.bind(slave_socket);
+    }
+
+    /// Interrupt source
+    IntMaster intsource;
+
 private:
+    /// TLM-2 master socket, defaults to 32-bits wide, base protocol
+    tlm_utils::simple_initiator_socket<Pl081> master_socket;
+
+    /** Generic payload transaction to use for master blocking requests.  This is used
+     * to speed up the simulation by not allocating dynamically a payload for
+     * each blocking transaction.
+     * @warn This prevents can only be used for blocking accesses
+     */
+    tlm::tlm_generic_payload master_b_pl;
+    
+    /** Time object for delay to use for master blocking requests.  This is used
+     * to speed up the simulation by not allocating dynamically a time object for
+     * each blocking transaction.
+     * @warn This can only be used for blocking accesses
+     */
+    sc_core::sc_time master_b_delay;
+
     /** Register read function
      * @param[in] offset Offset of the register to read
      * @return The value read
@@ -107,6 +156,11 @@ private:
 
         switch (index)
         {
+        case REG_PL081_DMACINTTCCLR:
+        case REG_PL081_DMACINTERRCLR:
+            TLM_ERR("read access to write-only register (%d)", index);
+            break;
+            
         default:
             // read the register value
             result = m_reg[index];
@@ -135,12 +189,52 @@ private:
 
         switch (index)
         {
+        case REG_PL081_DMACINTSTAT:
+        case REG_PL081_DMACINTTCSTAT:
+        case REG_PL081_DMACINTERRSTAT:
+        case REG_PL081_DMACRAWINTC:
+        case REG_PL081_DMACRAWINTERR:
+        case REG_PL081_DMACENBLDCHNS:
+        case REG_PL081_DMACPERIPHID0:
+        case REG_PL081_DMACPERIPHID1:
+        case REG_PL081_DMACPERIPHID2:
+        case REG_PL081_DMACPERIPHID3:
+        case REG_PL081_DMACPCELLID0:
+        case REG_PL081_DMACPCELLID1:
+        case REG_PL081_DMACPCELLID2:
+        case REG_PL081_DMACPCELLID3:
+            TLM_ERR("write access to read-only register (%d)", index);
+            break;
+
         default:
             m_reg[index] = value;
             break;
         }
     }
 
+    /** slave_socket non-blocking forward transport method (default behavior, can be overridden)
+     * @param[in, out] trans Transaction payload object, allocated here, filled by target
+     * @param[in, out] phase Phase payload object, allocated here
+     * @param[in, out] delay Time object, allocated here, filled by target
+     */
+    virtual tlm::tlm_sync_enum
+    master_nb_transport_bw(tlm::tlm_generic_payload& trans,
+                           tlm::tlm_phase& phase, sc_core::sc_time& delay)
+    {
+        SC_REPORT_FATAL("TLM-2", "Non blocking not implemented");
+        return tlm::TLM_COMPLETED;
+    }
+
+    /** master_socket tagged non-blocking forward transport method
+     * @param[in] start_range Start address of the memory invalidate command
+     * @param[in] end_range End address of the memory invalidate command
+     */
+    virtual void
+    master_invalidate_direct_mem_ptr(sc_dt::uint64 start_range,
+                                     sc_dt::uint64 end_range)
+    {
+        SC_REPORT_FATAL("TLM-2", "DMI not implemented");
+    }
 };
 
 #endif /*PL081_H_*/
